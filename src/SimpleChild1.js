f0ec99269de8ad2418af2c41ffaeb205f0813d43
Omar Rizwan <innovativewebpage@gmail.com>
2025-10-19 00:26:03 +0530

    remove <> tag

--- src/SimpleChild1.js
+++ src/SimpleChild1.js
@@ -1,24 +1,22 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 function SimpleChild1({ product, toggleFunction }) {
     return (
-        <>
-            <div style={{ border: "1px solid brown", padding: "20px" }}>
-                <p>ID: {product.id}</p>
-                <br />
-                <p>Name : {product.pName}</p>
-                <br />
-                <p>Price :{product.price}</p>
-                <br />
-                <p style={{ color: product.isAvailable ? "green" : "red" }}>
-                    {product.isAvailable ? "Available" : "Not-Available"}
-                </p>
+        <div style={{ border: "1px solid brown", padding: "20px" }}>
+            <p>ID: {product.id}</p>
+            <br />
+            <p>Name : {product.pName}</p>
+            <br />
+            <p>Price :{product.price}</p>
+            <br />
+            <p style={{ color: product.isAvailable ? "green" : "red" }}>
+                {product.isAvailable ? "Available" : "Not-Available"}
+            </p>
 
-                <button onClick={() => toggleFunction(product.isAvailable)}>
-                    Toggle
-                </button>
-            </div>
-        </>
+            <button onClick={() => toggleFunction(product.isAvailable)}>
+                Toggle
+            </button>
+        </div>
     );
 }
 
@@ -28,7 +26,7 @@ SimpleChild1.propTypes = {
         id: PropTypes.number,
         pName: PropTypes.string,
         price: PropTypes.number,
-        isAvailable: PropTypes.bool
+        isAvailable: PropTypes.bool,
     }),
-    toggleFunction: PropTypes.func
+    toggleFunction: PropTypes.func,
 };
