771ea18298cf90e8b75ab900b961150be3c6cd49
Omar Rizwan <innovativewebpage@gmail.com>
2025-10-26 20:23:06 +0530

    testing file is added

---
+++ src/Testing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Testing() {
+    return (
+        <>
+            <h3>{"Ilife"}</h3>
+            <p>{"infy"}</p>
+        </>
+    );
+}
+export default Testing;
